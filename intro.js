4d972d44954a44d5ec58c891dd1a310d4e916d90
pseboyeng@gmail.com <pseboyeng@gmail.com>
2022-06-24 09:22:05 +0200

    initial commit

---
+++ intro.js
@@ -0,0 +1,9 @@
+
+/*Introduction to Javascript.
+Print Name, Height, and Country.
+*/
+const mySelf=(firstName,lastName,height,country) => {
+    console.log(`Name : ${firstName} ${lastName}, Height : ${height}, Country : ${country}`);
+}
+
+mySelf("Peter","Seboyeng 😃",1.8,"South Africa");
